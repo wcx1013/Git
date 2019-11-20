0548713b6a69424c57ffeb2098b57e61cefa8866
cc <2744729611@qq.com>
2019-11-20 16:17:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.git;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initview();
+    }
+
+    private void initview() {
+        Log.e("tag","3434345");
+    }
+
+}
